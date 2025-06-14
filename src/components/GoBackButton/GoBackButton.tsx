4351cd6aa95549d091a8a5fd9c16ac20cd1b948e
Emerly <vadim200339@gmail.com>
2025-06-14 14:05:26 +0300

    added goback link

---
+++ src/components/GoBackButton/GoBackButton.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import type { MouseEvent } from "react";
+import { Link, useNavigate } from "react-router";
+import Text from "../../ui/Text/Text";
+import "./GoBackButton.css";
+
+type GoBackLinkProps = {
+  className?: string;
+};
+
+const GoBackButton = ({ className }: GoBackLinkProps) => {
+  const navigate = useNavigate();
+
+  function handleGoBack(e: MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    // 💡 Verify if previous page exists before using router.back
+    const hasPreviousPage = window.history.length > 2;
+
+    if (hasPreviousPage) {
+      navigate(-1);
+    } else {
+      navigate("..");
+    }
+  }
+
+  return (
+    <Link
+      to={".."}
+      onClick={handleGoBack}
+      className={clsx("go-back", className)}
+    >
+      <Text tag="span" color="light" size="regular">
+        Go Back
+      </Text>
+    </Link>
+  );
+};
+
+export default GoBackButton;
--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -7,6 +7,7 @@ import Title from "../../ui/Title/Title";
 import { formatKebabCase } from "../../utils/formatKebabCase";
 import { formatPrice } from "../../utils/formatPrice";
 import Counter from "../Counter/Counter";
+import GoBackButton from "../GoBackButton/GoBackButton";
 import ImageViewer from "../ImageViewer/ImageViewer";
 import "./ProductDetails.css";
 
@@ -32,50 +33,59 @@ const ProductDetails = ({ product }: Props) => {
   };
 
   return (
-    <div className="product-details">
-      <div className="product-details__images">
-        <ImageViewer images={product.images} />
-      </div>
-
-      <div className="product-details__content">
-        <Text
-          size="large"
-          color="text-primary"
-          weight="semibold"
-          className="product-details__category"
-        >
-          {formatKebabCase(product.category)}
-        </Text>
-        <Title
-          tag="h2"
-          size="xl"
-          weight="bold"
-          color="primary"
-          className="product-details__title"
-        >
-          {product.title}
-        </Title>
-        <Text
-          tag="p"
-          size="regular"
-          color="light"
-          className="product-details__description"
-        >
-          {product.description}
-        </Text>
-        <Text
-          size="xl"
-          color="text-primary"
-          weight="bold"
-          className="product-details__price"
-        >
-          ${formatPrice(product.price)}
-        </Text>
-        <div className="product-details__actions">
-          <Counter value={count} increment={increment} decrement={decrement} />
-          <button className="product-details__button" onClick={handleAddToCard}>
-            ADD TO CART
-          </button>
+    <div className="product-details__wrapper">
+      <GoBackButton className="product-details__back" />
+      <div className="product-details">
+        <div className="product-details__images">
+          <ImageViewer images={product.images} />
+        </div>
+        <div className="product-details__content">
+          <Text
+            size="large"
+            color="text-primary"
+            weight="semibold"
+            className="product-details__category"
+          >
+            {formatKebabCase(product.category)}
+          </Text>
+          <Title
+            tag="h2"
+            size="xl"
+            weight="bold"
+            color="primary"
+            className="product-details__title"
+          >
+            {product.title}
+          </Title>
+          <Text
+            tag="p"
+            size="regular"
+            color="light"
+            className="product-details__description"
+          >
+            {product.description}
+          </Text>
+          <Text
+            size="xl"
+            color="text-primary"
+            weight="bold"
+            className="product-details__price"
+          >
+            ${formatPrice(product.price)}
+          </Text>
+          <div className="product-details__actions">
+            <Counter
+              value={count}
+              increment={increment}
+              decrement={decrement}
+            />
+            <button
+              className="product-details__button"
+              onClick={handleAddToCard}
+            >
+              ADD TO CART
+            </button>
+          </div>
         </div>
       </div>
     </div>
